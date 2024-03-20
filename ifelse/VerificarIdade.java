ec53c2de803c0c8d70a0789715dfa6f314583013
unknown <db@DBS-1162.dbserver.com.br>
2024-03-20 12:11:08 -0300

    primeiro exercício da lista

---
+++ ifelse/VerificarIdade.java
@@ -0,0 +1,13 @@
+package ifelse;
+
+public class VerificarIdade {
+
+    // Definir a idade de uma pessoa e verificar se ela é maior de idade ou não.
+
+    public static void main(String[] args){
+        int idade = 10;
+
+        System.out.println((idade >= 18) ? "Maior de idade" : "Menor de idade");
+
+    }
+}
