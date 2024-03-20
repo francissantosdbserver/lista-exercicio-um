b9afa60b20ba332a465dc34301b77c79606ce6b6
unknown <db@DBS-1162.dbserver.com.br>
2024-03-20 18:50:36 -0300

    subindo exercicio 5

---
+++ lacofor/Tabuada.java
@@ -0,0 +1,13 @@
+package lacofor;
+
+public class Tabuada {
+
+    public static void main(String[] args){
+
+        int numero = 9;
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(numero + "x" + i + " = " + (numero * i));
+        }
+    }
+}
