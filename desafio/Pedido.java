bd6779d03c43b818c8c7a6191baa24765b727c49
unknown <db@DBS-1162.dbserver.com.br>
2024-03-24 20:03:59 -0300

    criando metodo deo do desafio

--- desafio/Pedido.java
+++ desafio/Pedido.java
@@ -2,15 +2,34 @@ package desafio;
 
 public class Pedido {
 
-    public void devolverTroco(double pagamento){
+    public void devolverTroco(double valorPago){
         if(pagamento >= imprimeValorTotal()){
-            troco = calculaTroco(pagamento);
+            troco = calculaTroco(valorPago);
             return troco;
         }
         System.out.println("Valor insuficiente");
     }
 
-    public void calculaTroco(double pagamento) {
-        pagamento - imprimeValorTotal();
+    public void calculaTroco(double valorPago) {
+        valorPago - imprimeValorTotal();
     }
+
+
+    double troco = devolverTroco(valorPago) != null ? devolverTroco(valorPago) : 0;
+
+    public void calculaNotas(double valorPago) {
+        double troco = valorPago - imprimeValorTotal();
+
+        int[] notas = {100, 50, 20, 10, 5, 2, 1};
+
+        System.out.println("Quantidade de notas:");
+        for (int nota : notas) {
+            int quantidadeNotas = (int) (troco / nota);
+            if (quantidadeNotas > 0) {
+                troco -= quantidadeNotas * nota;
+            }
+        }
+    }
+
+
 }
